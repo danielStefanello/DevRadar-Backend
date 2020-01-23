68e1cf5e97b3ba2bc335860c9dbbda10b1bb9a1c
Daniel Stefanello <danieldfst@hotmail.com>
2020-01-23 20:33:26 -0300

    Add update position of the Dev

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -47,4 +47,30 @@ module.exports = {
 
     return res.json(dev);
   },
+
+  async update(req, res) {
+    const { github_username, techs, latitude, longitude } = req.body;
+
+    let dev = await Dev.findOne({ github_username });
+
+    if (!dev) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const techsArray = parseStringAsArray(techs);
+
+    const location = {
+      type: 'Point',
+      coordinates: [longitude, latitude],
+    };
+
+    dev = await Dev.updateOne({
+      techs: techsArray,
+      location,
+    });
+
+    dev = await Dev.findOne({ github_username });
+
+    return res.json(dev);
+  },
 };
