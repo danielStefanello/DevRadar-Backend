2d2f725a7023e2208af4cc029402f863a7f529f8
Daniel Stefanello <danieldfst@hotmail.com>
2021-03-21 12:15:36 -0300

    Fix:update mongoose version and fix warnnings

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ mongoose.connect('mongodb://localhost:27017/mongo-dev-position', {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   useFindAndModify: true,
+  useCreateIndex: true,
 });
 
 // express sempre tem que vir antes do routes
